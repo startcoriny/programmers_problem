042e88ac9f8cb280d4b8177ad63fe3353a604a17
startcoriny <127919222+startcoriny@users.noreply.github.com>
2024-01-05 11:46:07 +0900

    [level unrated] Title: 각도기, Time: 0.02 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120829. 각도기/각도기.js
+++ 프로그래머스/unrated/120829. 각도기/각도기.js
@@ -1,15 +1,6 @@
 function solution(angle) {
 
-  let result;
-  if (angle === 180) {
-    result = 4;
-  } else if (angle === 90) {
-    result = 2
-  }else if (angle < 90) {
-    result = 1;
-  } else if (angle < 180) {
-    result = 3;
-  }
 
-  return result;
+  return angle < 90 ? 1 : angle === 90? 2 : angle < 180? 3: 4 ;
+
 }
