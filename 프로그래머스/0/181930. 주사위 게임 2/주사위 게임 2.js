646761be0878406791b6a30b0eeffde957d92db8
startcoriny <127919222+startcoriny@users.noreply.github.com>
2024-05-21 12:33:33 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
@@ -3,7 +3,7 @@ function solution(a, b, c) {
     if(a!==b && a!==c && b!==c){
         return answer = a+b+c
     }else if( a===b && b===c){
-        return answer = (a+b+c)*(a*a+b*b+c*c)*(a*a*a+b*b*b+c*c*c)
+        return answer = (a+b+c)*(a*a+b*b+c*c)*(a**3+b**3+c**3)
     }else if(a===b || a===c || b===c){
         return answer = (a+b+c)*(a*a+b*b+c*c)
     }
