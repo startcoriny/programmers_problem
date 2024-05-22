49c5fe213f1ee8200d966aaa807e1d85923788dd
startcoriny <127919222+startcoriny@users.noreply.github.com>
2024-05-22 11:28:51 +0900

    [level 0] Title: 수 조작하기 2, Time: 7.76 ms, Memory: 41.6 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.js
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.js
@@ -1,15 +1,4 @@
 function solution(numLog) {
-    // 1 - 0 = 1
-    // 0 - 1 = -1
-    // 10-0 = 10
-    // 0 - 10 = -10
-    // 1 - 0 = 1
-    // 0 - 1 = -1
-    // 10 - 0 = 10
-    // 0 - 10 = -10
-    // -1 - 0 = -1
-    // -2 - -1 = -1
-    // -1 - -2 = 1
     var answer = '';
     for(let i = 1; i <= numLog.length; i++){
         switch(numLog[i] - numLog[i-1] ){
