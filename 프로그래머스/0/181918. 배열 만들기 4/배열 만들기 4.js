2b5b3a4a12d93aa794bdcd711b8252ce843f7fa5
startcoriny <127919222+startcoriny@users.noreply.github.com>
2024-05-24 13:40:05 +0900

    [level 0] Title: 배열 만들기 4, Time: 2.96 ms, Memory: 37 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
@@ -0,0 +1,19 @@
+function solution(arr) {    
+    // stk.length === 0 => stk.push(arr[i]) => i++
+    // stk.length > 0 => stk[stk.length-1] < arr[i] => stk.push(arr[i]) => i++
+    // stk.length > 0 => stk[stk.length-1] >= arr[i] => stk.pop()
+    let stk = []
+    for(let i = 0; i < arr.length; i++){
+        if(stk.length === 0){
+            stk.push(arr[i])
+        }else{
+            if(stk[stk.length-1] < arr[i]){
+                stk.push(arr[i])
+            }else{
+                stk.pop()
+                i--
+            }
+        }
+    }
+    return stk
+}
