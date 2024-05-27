30d5c21ff2113ab7b42b54580177290968313773
startcoriny <127919222+startcoriny@users.noreply.github.com>
2024-05-27 15:37:27 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.js
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.js
@@ -1,24 +1,24 @@
-// function solution(my_string) {
-//     var answer = [];
-//     let eng = "ABCDEFGHIZKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-//     answer.length = 52
-//     answer.fill(0)
+function solution(my_string) {
+    let eng = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    let answer = [];
+    answer.length = 52;
+    answer.fill(0);
     
-//     my_string.split("").map(word=>{
-//         answer[eng.indexOf(word)] += 1
-//     })
-//     return answer;
-// }
-function solution(m) {
-    var answer = [];
-    let al = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    let a = [];
-    a.length = 52;
-    a.fill(0);
-
-    m.split("").map((n)=>{
-        a[al.indexOf(n)]+=1
+    my_string.split("").map(word=>{
+        answer[eng.indexOf(word)] += 1
     })
-
-    return a;
+    return answer;
 }
+// function solution(m) {
+//     var answer = [];
+//     let al = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+//     let a = [];
+//     a.length = 52;
+//     a.fill(0);
+
+//     m.split("").map((n)=>{
+//         a[al.indexOf(n)]+=1
+//     })
+
+//     return a;
+// }
