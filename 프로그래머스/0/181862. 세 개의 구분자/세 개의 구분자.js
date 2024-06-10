9b905f7d917f62dbdb399a8bd8d976470e2cc51f
startcoriny <127919222+startcoriny@users.noreply.github.com>
2024-06-10 22:49:16 +0900

    [level 0] Title: 세 개의 구분자, Time: 28.58 ms, Memory: 60.7 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.js
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.js
@@ -1,5 +1,5 @@
 function solution(myStr) {
-    let regexp = /(a|b|c)/g
+    let regexp = /[a|b|c]/g
     let arr = myStr.replaceAll(regexp, " ")
     let result = arr.split(" ").filter(str=>str !== "")
     return result.length === 0? ["EMPTY"]: result
