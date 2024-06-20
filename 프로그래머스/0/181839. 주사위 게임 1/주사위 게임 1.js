6e0a36a0311830e5a3210d701cec4fa2e199b8ab
Jimin Kwak <127919222+startcoriny@users.noreply.github.com>
2024-06-20 20:08:29 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.js
@@ -0,0 +1,14 @@
+function solution(a, b) {
+    var answer = 0;
+    
+    if(a % 2 !== 0 && b % 2 !== 0){
+        answer = Math.pow(a,2) + Math.pow(b,2)
+    }else if(a % 2 !== 0 || b % 2 !== 0){
+        answer = 2 * (a+b)
+    }else if(a % 2 === 0 && b % 2 === 0){
+        answer = Math.abs(a-b)
+    }
+    
+    
+    return answer;
+}
