60c385dea6ff3c7befd8a61a430c1a3a646713a4
Jimin Kwak <127919222+startcoriny@users.noreply.github.com>
2024-12-28 14:47:33 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -0,0 +1,10 @@
+function solution(my_string, num1, num2) {
+    let strArr = my_string.split("")
+    let temp = strArr[num1]
+    
+    strArr[num1] = strArr[num2]
+    strArr[num2] = temp
+    
+    return strArr.join("")
+    
+}
