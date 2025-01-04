9057438a81bee51bcb656810392a1fb7f0265f96
Jimin Kwak <127919222+startcoriny@users.noreply.github.com>
2025-01-04 15:49:42 +0900

    [level 0] Title: 로그인 성공?, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
@@ -0,0 +1,22 @@
+function solution(id_pw, db) {
+    
+    const idList = [];
+    
+    for(let [id,pw] of db){
+        if(id === id_pw[0]){
+            existId = true
+        }
+            
+        if(id === id_pw[0] && pw === id_pw[1]){
+            return "login"
+        }
+        
+        idList.push(id)
+    }
+    
+    if(idList.includes(id_pw[0])){
+        return "wrong pw"
+    }else {
+        return "fail"
+    }
+}
